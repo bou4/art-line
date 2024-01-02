e26a5572cd5ba638921e25ae8af5a95e0a5a5cd8
Caro Meysmans <caro.meysmans@ugent.be>
2024-01-02 20:37:13 +0100

    Update

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,13 @@
 // 1. Import utilities from `astro:content`
 import { z, reference, defineCollection } from 'astro:content';
+
 // 2. Define your collection(s)
 const artistCollection = defineCollection({
     type: 'content',
     schema: ({ image }) => z.object({
         name: z.string(),
         cover: image(),
+        order: z.number().default(0),
         education: z.array(z.string()).optional(),
         techniques: z.array(z.string()).optional(),
     }),
@@ -21,6 +23,7 @@ const artworkCollection = defineCollection({
         technique: z.string(),
     }),
 });
+
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
